b26e1abe06f4a110ca21e0085660919de3db197d
Michael Webster <miketwebster@gmail.com>
2013-10-12 21:08:45 -0400

    Make sure to hide the snap OSD when cancelling a drag with Escape
    Fixes linuxmint/cinnamon/#2450

--- src/core/keybindings.c
+++ src/core/keybindings.c
@@ -1835,8 +1835,8 @@ process_mouse_move_resize_grab (MetaDisplay *display,
   if (keysym == XK_Escape)
     {
       /* Hide the tiling preview if necessary */
-      if (window->tile_mode != META_TILE_NONE)
-        meta_screen_tile_preview_hide (screen);
+      meta_screen_tile_preview_hide (screen);
+      meta_screen_tile_hud_hide (screen);
 
       /* Restore the original tile mode */
       window->tile_mode = display->grab_tile_mode;
