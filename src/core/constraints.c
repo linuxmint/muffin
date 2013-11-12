5a0839a081736ac2add01b2c8f395ccabecbb4ee
Michael Webster <miketwebster@gmail.com>
2013-11-12 09:36:13 -0500

    Don't apply snapping rules to maximized windows when they're in a different workspace.
    Fixes linuxmint/cinnamon#2621

--- src/core/constraints.c
+++ src/core/constraints.c
@@ -809,7 +809,7 @@ constrain_maximization (MetaWindow         *window,
     return TRUE;
 
   /* Calculate target_size = maximized size of (window + frame) */
-  if (META_WINDOW_MAXIMIZED (window) && g_list_length (window->screen->active_workspace->snapped_windows) == 0)
+  if (META_WINDOW_MAXIMIZED (window) && g_list_length (window->workspace->snapped_windows) == 0)
     {
       target_size = info->work_area_monitor;
     }
