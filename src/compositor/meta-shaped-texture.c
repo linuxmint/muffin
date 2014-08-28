58b767c0f9b1e9edc9556781c6f09dd3dfc0e4d5
Owen W. Taylor <otaylor@fishsoup.net>
2014-08-28 15:18:23 -0400

    Use meta_actor_painting_untransformed() for MetaShapedTexture
    The old check for using NEAREST by checking
    clutter_actor_is_in_clone_paint()
    and meta_actor_is_untransformed (actor) doesn't work properly since
    clutter_actor_is_in_clone_paint() does not look at ancestors of the
    actor; it only applies to a direct clone of the actor. Using
    meta_actor_painting_untransformed() allows us to check exactly what we
    care about rather than using tricky approximations.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=735632

--- src/compositor/meta-shaped-texture.c
+++ src/compositor/meta-shaped-texture.c
@@ -488,15 +488,8 @@ meta_shaped_texture_paint (ClutterActor *actor)
 
   filter = COGL_PIPELINE_FILTER_LINEAR;
 
-  if (!clutter_actor_is_in_clone_paint (actor))
-    {
-      int x_origin, y_origin;
-
-      if (meta_actor_is_untransformed (actor,
-                                       &x_origin,
-                                       &y_origin))
-        filter = COGL_PIPELINE_FILTER_NEAREST;
-    }
+  if (meta_actor_painting_untransformed (tex_width, tex_height, NULL, NULL))
+    filter = COGL_PIPELINE_FILTER_NEAREST;
 
   ctx = clutter_backend_get_cogl_context (clutter_get_default_backend ());
   fb = cogl_get_draw_framebuffer ();
