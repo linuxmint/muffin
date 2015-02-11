20cecf0446955292d13fcb6a25bd1c42d6d4240b
Michael Webster <miketwebster@gmail.com>
2015-02-10 20:56:06 -0500

    screen.c: Update _NET_DESKTOP_NAMES when appropriate. Before we only did it when the MetaScreen was initialized - during startup - which would result in the server having the wrong names (or worse, the wrong length array of names.)

--- src/core/screen.c
+++ src/core/screen.c
@@ -1582,6 +1582,8 @@ meta_screen_remove_workspace (MetaScreen *screen, MetaWorkspace *workspace,
 
   update_net_desktop_layout (screen, new_num);
 
+  set_workspace_names (screen);
+
   /* If deleting a workspace before the current workspace, the active
    * workspace index changes, so we need to update that hint */
   if (active_index_changed)
@@ -1640,6 +1642,8 @@ meta_screen_append_new_workspace (MetaScreen *screen, gboolean activate,
     meta_prefs_set_num_workspaces (new_num);
 
   update_net_desktop_layout (screen, new_num);
+
+  set_workspace_names (screen);
 
   meta_screen_queue_workarea_recalc (screen);
 
@@ -1731,6 +1735,7 @@ update_num_workspaces (MetaScreen *screen,
 
   set_number_of_spaces_hint (screen, new_num);
   update_net_desktop_layout (screen, new_num);
+  set_workspace_names (screen);
 
   meta_screen_queue_workarea_recalc (screen);
 
