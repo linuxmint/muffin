8bdd4252b8e0b736c82a9bb0896bcb7d16964de2
Michael Webster <miketwebster@gmail.com>
2015-06-05 17:48:21 -0400

    screen.c: Fix toggle-desktop behavior.
    Whenever you restored windows, you would end up being one off in the
    focus list, always grabbing the next to last focused window, so you'd
    alternate between the last two windows when toggling.  This hack doesn't
    seem to be needed anymore.
    
    Fixes #4250

--- src/core/screen.c
+++ src/core/screen.c
@@ -3087,26 +3087,15 @@ meta_screen_toggle_desktop (MetaScreen *screen,
 {
   if (screen->active_workspace->showing_desktop)
     {
-      /*
-       * dirty hack because the actual window to be focused after unshowing the desktop
-       * lost its focus when showing desktop
-       * and therefore is second in the MRU list
-       */
-      MetaWindow *not_this_one;
-      not_this_one = meta_stack_get_default_focus_window(screen->stack, 
-                                                         screen->active_workspace,
-                                                         NULL);
       meta_screen_unshow_desktop (screen);
-      meta_workspace_focus_default_window (screen->active_workspace,
-                                           not_this_one,
-                                           timestamp);
-      // If there's only one window, make sure it gets the focus
       meta_workspace_focus_default_window (screen->active_workspace,
                                            NULL,
                                            timestamp);
     }
   else
-    meta_screen_show_desktop (screen, timestamp);
+    {
+      meta_screen_show_desktop (screen, timestamp);
+    }
 }
 
 void
