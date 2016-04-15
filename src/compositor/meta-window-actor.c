76fcd6e8d7cfc34d170f123c9e68c9e6f5f356ec
itzexor <itzexor@gmail.com>
2016-04-15 14:38:48 -0700

    Revert "solve cinnamon #681"
    This reverts commit 9bda8d2899a41809621ef9bef6db0c94d7ca0f02.

--- src/compositor/meta-window-actor.c
+++ src/compositor/meta-window-actor.c
@@ -1664,8 +1664,6 @@ meta_window_actor_new (MetaWindow *window)
   MetaWindowActorPrivate *priv;
   MetaFrame		 *frame;
   Window		  top_window;
-  MetaRectangle rectWorkArea[1];
-  MetaRectangle *rectWindow;
 
   frame = meta_window_get_frame (window);
   if (frame)
@@ -1708,25 +1706,6 @@ meta_window_actor_new (MetaWindow *window)
       window->type == META_WINDOW_POPUP_MENU ||
       window->type == META_WINDOW_COMBO) {
     clutter_container_add_actor (CLUTTER_CONTAINER (info->top_window_group),
-			       CLUTTER_ACTOR (self));
-  }
-  else if (window->type == META_WINDOW_TOOLTIP) {
-    meta_window_get_work_area_all_monitors(window, rectWorkArea);
-    rectWindow = meta_window_get_rect(window);
-    // move tooltip out of top panel if necessary
-    if (rectWindow->y < rectWorkArea->y) {
-      meta_window_move(window, FALSE, rectWindow->x, rectWorkArea->y);
-    }
-    rectWindow = meta_window_get_rect(window);
-    // move tooltip out of bottom panel if necessary
-    if ((rectWindow->y + rectWindow->height) > (rectWorkArea->y  + rectWorkArea->height)) {
-      meta_window_move(window, FALSE, rectWindow->x, rectWorkArea->y + rectWorkArea->height - rectWindow->height);
-    }
-    clutter_container_add_actor (CLUTTER_CONTAINER (info->top_window_group),
-		       CLUTTER_ACTOR (self));
-  }
-  else if (window->type == META_WINDOW_DESKTOP) {
-    clutter_container_add_actor (CLUTTER_CONTAINER (info->bottom_window_group),
 			       CLUTTER_ACTOR (self));
   }else{
     clutter_container_add_actor (CLUTTER_CONTAINER (info->window_group),
