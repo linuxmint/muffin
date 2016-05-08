28f5be59f3e4a99b4a8539a36b4fe9b8b6686d09
Michael Webster <miketwebster@gmail.com>
2016-05-07 21:13:16 -0400

    window.c: Handle META_TILE_MAXIMIZE in update_tile_mode, and move its call to handle both moving and resizing.  This prevents strange resize behavior when you move a window to an upper tile region without tiling it. Its tile state becomes inconsistent, and an attempt to resize it vertically ends up resizing in the wrong (or both) directions.

--- src/core/window.c
+++ src/core/window.c
@@ -9944,6 +9944,7 @@ update_tile_mode (MetaWindow *window)
       case META_TILE_LRC:
       case META_TILE_TOP:
       case META_TILE_BOTTOM:
+      case META_TILE_MAXIMIZE:
           if (!META_WINDOW_TILED_OR_SNAPPED (window))
               window->tile_mode = META_TILE_NONE;
           break;
@@ -10096,18 +10097,19 @@ meta_window_handle_mouse_grab_op_event (MetaWindow *window,
                                event->xbutton.x_root,
                                event->xbutton.y_root,
                                TRUE);
-
-              /* If a tiled window has been dragged free with a
-               * mouse resize without snapping back to the tiled
-               * state, it will end up with an inconsistent tile
-               * mode on mouse release; cleaning the mode earlier
-               * would break the ability to snap back to the tiled
-               * state, so we wait until mouse release.
-               */
-              if (window->tile_type == META_WINDOW_TILE_TYPE_NONE)
-                update_tile_mode (window);
             }
+
+          /* If a tiled window has been dragged free with a
+           * mouse resize without snapping back to the tiled
+           * state, it will end up with an inconsistent tile
+           * mode on mouse release; cleaning the mode earlier
+           * would break the ability to snap back to the tiled
+           * state, so we wait until mouse release.
+           */
+          if (window->tile_type == META_WINDOW_TILE_TYPE_NONE)
+            update_tile_mode (window);
         }
+
       window->maybe_retile_maximize = FALSE;
       meta_display_end_grab_op (window->display, event->xbutton.time);
       break;
