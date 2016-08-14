9a000359bbeebcedf66352d6f09661902ec56839
itzexor <itzexor@gmail.com>
2016-08-13 20:35:19 -0700

    meta-window-actor: Change unredirection hints to match spec changes
    Change the bypass / dont_bypass compositor code to match the latest spec.
    
    https://github.com/GNOME/mutter/commit/9a576265561ba956798d2a71721c0477d415ff2f

--- src/core/window-private.h
+++ src/core/window-private.h
@@ -95,6 +95,12 @@ enum {
 #define HUD_WIDTH 24
 #define CSD_TITLEBAR_HEIGHT 48
 
+typedef enum {
+  _NET_WM_BYPASS_COMPOSITOR_HINT_AUTO = 0,
+  _NET_WM_BYPASS_COMPOSITOR_HINT_ON = 1,
+  _NET_WM_BYPASS_COMPOSITOR_HINT_OFF = 2,
+} MetaBypassCompositorHintValue;
+
 struct _MetaWindow
 {
   GObject parent_instance;
@@ -480,8 +486,7 @@ struct _MetaWindow
   MetaWindow *tile_match;
 
   /* Bypass compositor hints */
-  guint bypass_compositor : 1;
-  guint dont_bypass_compositor : 1;
+  guint bypass_compositor;
 };
 
 struct _MetaWindowClass
--- src/core/window.c
+++ src/core/window.c
@@ -3688,7 +3688,7 @@ meta_window_is_on_primary_monitor (MetaWindow *window)
 gboolean
 meta_window_requested_bypass_compositor (MetaWindow *window)
 {
-  return window->bypass_compositor;
+  return window->bypass_compositor == _NET_WM_BYPASS_COMPOSITOR_HINT_ON;
 }
 
 /**
@@ -3699,7 +3699,7 @@ meta_window_requested_bypass_compositor (MetaWindow *window)
 gboolean
 meta_window_requested_dont_bypass_compositor (MetaWindow *window)
 {
-  return window->dont_bypass_compositor;
+  return window->bypass_compositor == _NET_WM_BYPASS_COMPOSITOR_HINT_OFF;
 }
 
 static void
