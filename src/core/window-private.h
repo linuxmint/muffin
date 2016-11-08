a3f06f77b3bc40780f4ed88f6b6bfe4db47e30f3
lestcape <lestcape@gmail.com>
2016-11-07 19:21:52 -0600

    Support GTK+'s hide-titlebar-when-maximized hint: https://mail.gnome.org/archives/commits-list/2011-December/msg05309.html (Fixed: https://github.com/linuxmint/muffin/issues/268).

--- src/core/window-private.h
+++ src/core/window-private.h
@@ -146,7 +146,8 @@ struct _MetaWindow
   char *gtk_window_object_path;
   char *gtk_app_menu_object_path;
   char *gtk_menubar_object_path;
-  
+
+  int hide_titlebar_when_maximized;
   int net_wm_pid;
   
   Window xtransient_for;
