ad4dbc7712e1e61b83623e2619ec718cee6da734
Jason Hicks <jaszhix@gmail.com>
2019-03-18 07:09:58 -0500

    screen: Ensure monitors update on monitor change (#439)

--- src/core/screen.c
+++ src/core/screen.c
@@ -3065,7 +3065,12 @@ meta_screen_resize (MetaScreen *screen,
       MetaWindow *window = tmp->data;
 
       if (window->screen == screen)
-        meta_window_update_for_monitors_changed (window);
+        {
+          meta_window_update_for_monitors_changed (window);
+
+          if (!window->override_redirect)
+            meta_window_update_monitor (window);
+        }
     }
 
   g_free (old_monitor_infos);
