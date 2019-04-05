65ffb8f10f6140bfeb6e93d3d6e13322febfa8c7
Jason Hicks <jaszhix@gmail.com>
2019-04-05 07:00:53 -0500

    compositor: Avoid changing pipeline/source if shadow is not being painted (#459)
    Avoids some context invalidations in cogl.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=782344
    
    
    (cherry picked from commit 3c6a518d40d0ed8d04343f0780100e88f8519ed1)

--- src/compositor/meta-shadow-factory.c
+++ src/compositor/meta-shadow-factory.c
@@ -215,9 +215,7 @@ meta_shadow_paint (MetaShadow     *shadow,
   int dest_x[4];
   int dest_y[4];
   int n_x, n_y;
-
-  cogl_pipeline_set_color4ub (shadow->pipeline,
-                              opacity, opacity, opacity, opacity);
+  gboolean source_updated = FALSE;
 
   cogl_set_source (shadow->pipeline);
 
@@ -294,6 +292,17 @@ meta_shadow_paint (MetaShadow     *shadow,
             overlap = cairo_region_contains_rectangle (clip, &dest_rect);
           else
             overlap = CAIRO_REGION_OVERLAP_IN;
+
+          if (overlap == CAIRO_REGION_OVERLAP_OUT)
+            continue;
+
+          if (!source_updated)
+            {
+              cogl_pipeline_set_color4ub (shadow->pipeline,
+                                          opacity, opacity, opacity, opacity);
+              cogl_set_source (shadow->pipeline);
+              source_updated = TRUE;
+            }
 
           /* There's quite a bit of overhead from allocating a new
            * region in order to find an exact intersection and
