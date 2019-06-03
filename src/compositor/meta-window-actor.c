92dd2b990427a93c282501e450a0d886ef7dffed
Jason Hicks <jaszhix@gmail.com>
2019-06-03 04:15:40 -0500

    window-actor: Support applying minimize effect

--- src/compositor/meta-window-actor.c
+++ src/compositor/meta-window-actor.c
@@ -2223,7 +2223,7 @@ meta_window_actor_hide (MetaWindowActor *self,
     start_simple_effect (self, event);
 
   /* Hide the actor immediately, Cinnamon will clone it and continue the effect with the clone. */
-  if (event != META_PLUGIN_DESTROY)
+  if (event != META_PLUGIN_DESTROY && event != META_PLUGIN_MINIMIZE)
     clutter_actor_hide (CLUTTER_ACTOR (self));
 }
 
