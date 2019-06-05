1eb5a517988de2f5485f0eec45b5a9ccb441b46b
Jason Hicks <jaszhix@gmail.com>
2019-06-05 06:34:13 -0500

    window-actor: Fix segfault in meta_window_actor_should_unredirect

--- src/compositor/meta-window-actor.c
+++ src/compositor/meta-window-actor.c
@@ -1945,6 +1945,9 @@ meta_window_actor_should_unredirect (MetaWindowActor *self)
   MetaWindowActorPrivate *priv = self->priv;
   MetaWindow *window = priv->window;
 
+  if (!window)
+    return FALSE;
+
   if (meta_window_actor_is_destroyed (self))
     return FALSE;
 
