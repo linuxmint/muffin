68cf5f36bfda6c256f33d6e9494b31df25522538
Simon Brown <brownsr@users.noreply.github.com>
2019-06-07 16:13:41 +0100

    compositor: fix a few compiler warnings (#492)

--- src/compositor/compositor.c
+++ src/compositor/compositor.c
@@ -220,11 +220,9 @@ get_output_window (MetaScreen *screen)
 {
   MetaDisplay *display = meta_screen_get_display (screen);
   Display     *xdisplay = meta_display_get_xdisplay (display);
-  Window       output, xroot;
+  Window       output;
   XWindowAttributes attr;
   long         event_mask;
-
-  xroot = meta_screen_get_xroot (screen);
 
   event_mask = FocusChangeMask |
                ExposureMask |
@@ -830,9 +828,9 @@ is_grabbed_event (XEvent *event)
     case KeyPress:
     case KeyRelease:
       return TRUE;
+    default:
+      return FALSE;
     }
-
-  return FALSE;
 }
 
 void
@@ -1022,6 +1020,8 @@ sync_actor_stacking (MetaCompositor *compositor)
   GList *tmp;
   GList *old;
   gboolean reordered;
+  ClutterActor *parent;
+  gboolean popup_window_visible;
 
   /* NB: The first entries in the lists are stacked the lowest */
 
@@ -1075,8 +1075,7 @@ sync_actor_stacking (MetaCompositor *compositor)
   if (!reordered)
     return;
 
-  ClutterActor *parent;
-  gboolean popup_window_visible = FALSE;
+  popup_window_visible = FALSE;
 
   for (tmp = g_list_last (compositor->windows); tmp != NULL; tmp = tmp->prev)
     {
@@ -1306,7 +1305,6 @@ meta_pre_paint_func (gpointer data)
 {
   GList *l;
   MetaCompositor *compositor = data;
-  GSList *screens = compositor->display->screens;
   MetaWindowActor *top_window_actor = NULL;
   MetaWindowActor *expected_unredirected_window = NULL;
 
