3063157aee736cf5b0f1ff382a037bb912254e66
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2019-06-10 17:34:23 +0100

    Don't set CLUTTER_ACTOR_NO_LAYOUT flag
    Fixes the flydown issue in https://github.com/linuxmint/alpha-testing/issues/2.
    
    This could also be responsible for flickering regressions in upgrades from 3.8 to 4.0 (where this flag is set in init())
    and from 4.0.6 to 4.0.7 (where it's set in init and removed during window grabbing).

--- src/compositor/compositor-private.h
+++ src/compositor/compositor-private.h
@@ -77,9 +77,6 @@ void     meta_end_modal_for_plugin   (MetaScreen       *screen,
 gint64 meta_compositor_monotonic_time_to_server_time (MetaDisplay *display,
                                                       gint64       monotonic_time);
 
-void meta_compositor_grab_op_begin (MetaCompositor *compositor);
-void meta_compositor_grab_op_end (MetaCompositor *compositor);
-
 void meta_check_end_modal (MetaScreen *screen);
 
 #endif /* META_COMPOSITOR_PRIVATE_H */
