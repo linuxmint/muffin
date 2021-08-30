622325b2193b7e938450626b6cd046f05e0bf4ff
JosephMcc <mccullarjoseph@gmail.com>
2021-08-30 12:38:08 -0700

    theme.c: Rename button_rect() to get_button_rect()

--- src/ui/theme.c
+++ src/ui/theme.c
@@ -4575,10 +4575,10 @@ meta_frame_style_validate (MetaFrameStyle    *style,
 }
 
 static void
-button_rect (MetaButtonType           type,
-             const MetaFrameGeometry *fgeom,
-             int                      middle_background_offset,
-             GdkRectangle            *rect)
+get_button_rect (MetaButtonType           type,
+                 const MetaFrameGeometry *fgeom,
+                 int                      middle_background_offset,
+                 GdkRectangle            *rect)
 {
   switch (type)
     {
@@ -4850,7 +4850,7 @@ meta_frame_style_draw_with_style (MetaFrameStyle          *style,
             {
               MetaButtonState button_state;
 
-              button_rect (j, fgeom, middle_bg_offset, &rect);
+              get_button_rect (j, fgeom, middle_bg_offset, &rect);
 
               button_state = map_button_state (j, fgeom, middle_bg_offset, button_states);
 
