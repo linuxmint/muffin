7f96d553ab97b09620d63bd481fcff2270b73a8a
Michael Webster <miketwebster@gmail.com>
2022-01-27 09:45:34 -0500

    Make meta_window_get_client_pid() public.

--- src/core/window-private.h
+++ src/core/window-private.h
@@ -835,8 +835,6 @@ gboolean meta_window_handle_ui_frame_event (MetaWindow         *window,
 void meta_window_handle_ungrabbed_event (MetaWindow         *window,
                                          const ClutterEvent *event);
 
-uint32_t meta_window_get_client_pid (MetaWindow *window);
-
 void meta_window_get_client_area_rect (const MetaWindow      *window,
                                        cairo_rectangle_int_t *rect);
 void meta_window_get_titlebar_rect (MetaWindow    *window,
