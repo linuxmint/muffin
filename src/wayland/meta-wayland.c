505c1ef0e0f685351a1e13966b27e9dc9298b19c
Sunderland93 <samoilov.lex@gmail.com>
2025-08-14 09:44:09 +0400

    init idle_inhibit

--- src/wayland/meta-wayland.c
+++ src/wayland/meta-wayland.c
@@ -439,6 +439,7 @@ meta_wayland_compositor_setup (MetaWaylandCompositor *wayland_compositor)
   meta_wayland_surface_inhibit_shortcuts_dialog_init ();
   meta_wayland_text_input_init (compositor);
   meta_wayland_gtk_text_input_init (compositor);
+  meta_wayland_idle_inhibit_init (compositor);
 
   /* Xwayland specific protocol, needs to be filtered out for all other clients */
   if (meta_xwayland_grab_keyboard_init (compositor))
