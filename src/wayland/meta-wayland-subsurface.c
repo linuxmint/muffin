e55d0a0faee9567ed376fc73b05efa1656eba698
Sunderland93 <samoilov.lex@gmail.com>
2025-09-20 21:49:25 +0400

    Fix build

--- src/wayland/meta-wayland-subsurface.c
+++ src/wayland/meta-wayland-subsurface.c
@@ -147,7 +147,7 @@ meta_wayland_subsurface_union_geometry (MetaWaylandSubsurface *subsurface,
     .height = meta_wayland_surface_get_height (surface),
   };
 
-  if (surface->buffer_ref.buffer)
+  if (surface->buffer_ref->buffer)
     meta_rectangle_union (out_geometry, &geometry, out_geometry);
 
   META_WAYLAND_SURFACE_FOREACH_SUBSURFACE (surface, subsurface_surface)
