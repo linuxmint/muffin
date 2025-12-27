d05bc19e8177d74366d7a2c493670bdde723703b
Aleksey Samoilov <samoilov.lex@gmail.com>
2025-12-27 22:16:36 +0400

    clutter/stage-cogl: Avoid copying fb_clip_region

--- clutter/clutter/cogl/clutter-stage-cogl.c
+++ clutter/clutter/cogl/clutter-stage-cogl.c
@@ -743,7 +743,7 @@ clutter_stage_cogl_redraw_view (ClutterStageWindow *stage_window,
    * artefacts.
    */
   if (use_clipped_redraw)
-    swap_region = cairo_region_copy (fb_clip_region);
+    swap_region = cairo_region_reference (fb_clip_region);
   else
     swap_region = cairo_region_create ();
 
