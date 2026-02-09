f378f68d76704fed92bdee226483f7f0de33e87d
Michael Webster <miketwebster@gmail.com>
2026-02-08 21:05:04 -0500

    meta-wayland-pointer.c: Get correct device coordinates from the stage.
    The stage->pointer_devices table does not currently get populated
    (TODO) so pos.x and .y were never getting anything assigned to
    them.
    
    Fixes linuxmint/wayland#93, Fixes linuxmint/wayland#106
    Fixes linuxmint/wayland#115, Fixes linuxmint/wayland#116
    
    Maybe fixes?
    linuxmint/wayland#164 (works on 147.0.3)
    linuxmint/wayland#169

--- src/wayland/meta-wayland-pointer.c
+++ src/wayland/meta-wayland-pointer.c
@@ -954,7 +954,6 @@ meta_wayland_pointer_set_focus (MetaWaylandPointer *pointer,
 
   if (surface != NULL)
     {
-      ClutterStage *stage = CLUTTER_STAGE (meta_backend_get_stage (backend));
       struct wl_client *client = wl_resource_get_client (surface->resource);
       graphene_point_t pos;
       MetaWindow *focus_window;
@@ -966,7 +965,7 @@ meta_wayland_pointer_set_focus (MetaWaylandPointer *pointer,
                                 G_CALLBACK (focus_surface_destroyed),
                                 pointer);
 
-      clutter_stage_get_device_coords (stage, pointer->device, NULL, &pos);
+      clutter_input_device_get_coords (pointer->device, NULL, &pos);
 
       focus_window = meta_wayland_surface_get_window (pointer->focus_surface);
       if (focus_window)
@@ -1071,12 +1070,10 @@ meta_wayland_pointer_get_relative_coordinates (MetaWaylandPointer *pointer,
 					       wl_fixed_t         *sx,
 					       wl_fixed_t         *sy)
 {
-  MetaBackend *backend = meta_get_backend ();
-  ClutterStage *stage = CLUTTER_STAGE (meta_backend_get_stage (backend));
   float xf = 0.0f, yf = 0.0f;
   graphene_point_t pos;
 
-  clutter_stage_get_device_coords (stage, pointer->device, NULL, &pos);
+  clutter_input_device_get_coords (pointer->device, NULL, &pos);
   meta_wayland_surface_get_relative_coordinates (surface, pos.x, pos.y, &xf, &yf);
 
   *sx = wl_fixed_from_double (xf);
